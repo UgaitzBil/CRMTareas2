a968b0550d6c9f59eaa9d6cbea684f7993514cdc
UgaitzBil <ubilbao13@gmail.com>
2025-12-04 21:31:01 +0100

    cuarto commit

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,5 +1,4 @@
-// pages/api/projects.js
-import { supabase } from "../../lib/supabase";
+import { supabase } from '../../lib/supabase';
 
 export default async function handler(req, res) {
   try {
